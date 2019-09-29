5c0b9bbe443737e731e211287e46f45a9cb64c52
prashdeep <pradeep.kumar44@gmail.com>
2019-09-30 00:09:15 +0530

    commit with class based component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class  App extends Component {
       <div className="App">
         <Navbar/>
         <div className="container">
-          <Users />   
+          <Users/>   
         </div>
       </div>
     );
